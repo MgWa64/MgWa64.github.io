3c51296b4e08679e538a29871db296c529fe865a
MgWa64 <35355800+MgWa64@users.noreply.github.com>
2018-02-26 21:08:29 -0800

    Update spells.js

--- docs/spells.js
+++ docs/spells.js
@@ -1,1 +1,4 @@
-
+console.log("Hello, World.");
+document.getElementById("SpellButtonPress").onclick = function(){
+                document.getElementById("SpellDesc").innerText = "Hello, World.";
+};
