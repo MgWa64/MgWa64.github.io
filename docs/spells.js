6e1914dc05e25ab56ac37293ff0b5602035248e3
MgWa64 <35355800+MgWa64@users.noreply.github.com>
2018-03-16 19:08:14 -0700

    Delete spells.js

--- docs/spells.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello, World.");
-document.getElementById("SpellButtonPress").onclick = function(){
-                document.getElementById("SpellDesc").innerText = "Hello, World.";
-};
